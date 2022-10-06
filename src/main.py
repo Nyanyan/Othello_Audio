e1691b33e17c5ba39b6d34b165e03c20116fd65f
Nyanyan <nyanyama3141pi2@yahoo.co.jp>
2022-10-06 13:38:05 +0900

    update

--- src/main.py
+++ src/main.py
@@ -2,28 +2,22 @@ import pyaudio
 import json
 import numpy as np
 
-RATE = 44100
-
-BPM = 100
-L1 = (60 / BPM * 4)
-L2,L4,L8 = (L1/2,L1/4,L1/8)
-
-C,D,E,F,G,A,B,C2 = (
-        261.626, 293.665, 329.628, 
-        349.228, 391.995, 440.000,
-        493.883, 523.251)
+rate = 44100
 
 def tone(freq, length, gain):
-    slen = int(length * RATE)
-    t = float(freq) * np.pi * 2 / RATE
+    slen = int(length * rate)
+    t = float(freq) * np.pi * 2.0 / rate
     return np.sin(np.arange(slen) * t) * gain
+
+def tone_rich(freq, length, gain):
+    return tone(freq, length, gain * 0.7) + tone(freq * 2, length, gain * 0.2) + tone(freq * 4, length, gain * 0.1)
 
 def play_wave(stream, samples):
     stream.write(samples.astype(np.float32).tobytes())
 
 def frequency(value):
-    value += 64
-    return 100 + value * 2
+    base_freq = 440.00
+    return base_freq * 2.0 ** (value / 12)
 
 with open('2022_10_02_14_15_33.json', encoding='utf-8-sig') as f:
     raw_data = json.load(f)
@@ -36,10 +30,10 @@ print(data)
 
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paFloat32, channels=1, rate=RATE, frames_per_buffer=1024, output=True)
+stream = p.open(format=pyaudio.paFloat32, channels=1, rate=rate, frames_per_buffer=1024, output=True)
 
 for datum in data:
-    play_wave(stream, tone(frequency(datum), 0.1, 1.0))
-    print(datum)
+    play_wave(stream, tone_rich(frequency(datum), 0.1, 1.0))
+    print(datum, frequency(datum))
 
 stream.close()
